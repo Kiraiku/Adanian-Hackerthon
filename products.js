bd6fe6acfce19f369fd434727ea5fe733cb177e6
Kiraiku <wanjauperminus@gmail.com>
2022-03-12 14:25:27 +0300

    cart

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Greatlap l9",
+      desc : "Greatest properly off ham exercise all.",
+      img : "dummy-pdt-b.jpg",
+      price : 22333
+    },
+    124: {
+      name : "Music System",
+      desc : "Unsatiable its possession nor off.",
+      img : "phone.jpg",
+      price : 7001
+    },
+    125: {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img :   "phone2.jpg",
+      price : 6750
+    },
+    126: {
+      name : "Kajuithi",
+      desc : "Offer! offer! offer!. ",
+      img : "juice.jpg",
+      price : 99
+    }
+  };
